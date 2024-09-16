791d99169d21f1d8330549f5afc3ab8a69d0e04e
ryo-i <iidazz0109@gmail.com>
2024-09-17 07:40:19 +0900

    ベースパスを変更

--- openapigen/runtime.ts
+++ openapigen/runtime.ts
@@ -13,7 +13,7 @@
  */
 
 
-export const BASE_PATH = "http://petstore.swagger.io/v1".replace(/\/+$/, "");
+export const BASE_PATH = "/v1".replace(/\/+$/, "");
 
 export interface ConfigurationParameters {
     basePath?: string; // override base path
