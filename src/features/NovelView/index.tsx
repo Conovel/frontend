a7b16ebe92272a67c24f880b9151ff4d05b0a7ac
Yuya757 <pantera.yk37@gmail.com>
2024-10-21 00:14:26 +0900

    wip; ボタン改善した

--- src/features/NovelView/index.tsx
+++ src/features/NovelView/index.tsx
@@ -1,5 +1,7 @@
 import { FaThumbsUp } from 'react-icons/fa';
 import ChatIcon from '@mui/icons-material/Chat';
+import AddCircleIcon from '@mui/icons-material/AddCircle';
+import DragIndicatorIcon from '@mui/icons-material/DragIndicator';
 import { styles } from './styles';
 
 export const NovelView = function () {
@@ -54,21 +56,28 @@ export const NovelView = function () {
           </div>
         </div>
       </div>
+      <div style={styles.verticalLine}></div>
       <div>
         <div style={styles.mainPanel}>
           <p>主人の帰りが何よりも心待ちだったことを思い出す。</p>
           <p>今思えば、なんてちっぽけなことに心躍らせていたのだろう。</p>
+          <DragIndicatorIcon style={styles.dragIndicatorIcon} />
         </div>
       </div>
 
-      <div style={styles.footer}>
+      <div style={styles.nextPanel}>
         <div style={styles.contentWrapper}>
-          <div style={styles.postBox}>
-            <button style={styles.button}>
-              続きを
-              <br />
-              自分で書く
-            </button>
+          <div>
+            <div style={styles.postBox}>
+              <AddCircleIcon style={styles.icon} />
+              <div style={styles.buttonWrapper}>
+                <button style={styles.button}>
+                  続きを
+                  <br />
+                  自分で書く
+                </button>
+              </div>
+            </div>
           </div>
 
           <div>
--- src/features/NovelView/styles.tsx
+++ src/features/NovelView/styles.tsx
@@ -1,16 +1,20 @@
 export const styles: { [key: string]: React.CSSProperties } = {
   frame: {
     position: 'absolute',
-    margin: '5vw',
-    top: '40px',
+    top: '8vh',
     height: '90vh',
   },
   contentWrapper: {
     display: 'flex',
     justifyContent: 'space-between',
     width: '100%',
+    zIndex: 2,
   },
   postBox: {
+    display: 'flex',
+    alignItems: 'center',
+    width: '30vw',
+    boxSizing: 'border-box',
     border: '1px solid #000',
     borderRadius: '10px',
     flex: '1 1 30%',
@@ -24,28 +28,54 @@ export const styles: { [key: string]: React.CSSProperties } = {
     textAlign: 'center',
   },
   icon: {
-    marginRight: '5px',
+    fontSize: '20px',
+  },
+  verticalLine: {
+    position: 'absolute',
+    top: '8vh',
+    bottom: '10vh',
+    left: '50%',
+    width: '1px',
+    backgroundColor: '#000',
+    zIndex: 1,
   },
   mainPanel: {
+    position: 'relative',
     textAlign: 'left',
     border: '1px solid #000',
     margin: '15vh 0',
     height: '30vh',
     alignItems: 'center',
-    paddingTop: '50px',
     fontSize: '1.2rem',
+    zIndex: 2,
   },
-  footer: {
+
+  dragIndicatorIcon: {
+    fontSize: '20px',
     position: 'absolute',
-    margin: '5vw',
-    top: '80vh',
+    bottom: '10px',
+    right: '10px',
   },
+
+  nextPanel: {
+    position: 'absolute',
+    top: '80vh',
+    zIndex: 2,
+  },
+  buttonWrapper: {
+    flex: 1,
+    display: 'flex',
+    justifyContent: 'center',
+  },
+
   button: {
+    width: '90%',
     padding: '5px 10px',
     backgroundColor: '#000',
     color: '#fff',
     border: 'none',
     borderRadius: '5px',
+    textAlign: 'left',
   },
   footerReactions: {
     display: 'flex',
