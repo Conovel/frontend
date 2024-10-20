4fcf45e7bf4ee29df354c038306791764c01f91b
azufb <curiousazunas@gmail.com>
2024-10-21 01:01:04 +0900

    とりあえず内容実装

--- src/features/Company/CompanyPresenter.tsx
+++ src/features/Company/CompanyPresenter.tsx
@@ -1,7 +1,13 @@
+import Box from '@mui/material/Box';
+
 export const CompanyPresenter = () => {
   return (
     <>
       <h2>運営会社</h2>
+      <Box>
+        <p>合同会社Technokuro</p>
+        <p>http://techno-kuro.com/</p>
+      </Box>
     </>
   );
 };
