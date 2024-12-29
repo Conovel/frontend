2069fc66cadf49a2424c73632c746637a851801d
azufb <curiousazunas@gmail.com>
2024-12-29 12:12:10 +0900

    とりあえずアカウント情報配置

--- src/features/AccountSettings/AccountSettingsPresenter.tsx
+++ src/features/AccountSettings/AccountSettingsPresenter.tsx
@@ -1,3 +1,5 @@
+import Box from '@mui/material/Box';
+import Switch from '@mui/material/Switch';
 import Typography from '@mui/material/Typography';
 
 export const AccountSettingsPresenter = () => {
@@ -6,6 +8,29 @@ export const AccountSettingsPresenter = () => {
       <Typography variant='h4'>アカウント情報</Typography>
 
       {/** アカウント情報 */}
+      <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+        {/** アバター */}
+
+        <Box sx={{ display: 'flex', alignItems: 'center' }}>
+          <Typography>ペンネーム</Typography>
+          <Typography>花子</Typography>
+        </Box>
+
+        <Box sx={{ display: 'flex', alignItems: 'center' }}>
+          <Typography>ニックネーム</Typography>
+          <Typography>HANA</Typography>
+        </Box>
+
+        <Box sx={{ display: 'flex', alignItems: 'center' }}>
+          <Typography>生年月</Typography>
+          <Typography>1998/2</Typography>
+        </Box>
+
+        <Box sx={{ display: 'flex', alignItems: 'center' }}>
+          <Typography>匿名設定</Typography>
+          <Switch />
+        </Box>
+      </Box>
 
       {/** アカウント削除ボタン */}
 
