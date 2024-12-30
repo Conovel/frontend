71149b77acbc7ee2ab9421ed07221d0491720a02
azufb <curiousazunas@gmail.com>
2024-12-30 11:41:32 +0900

    配置調整

--- src/features/AccountSettings/AccountSettingsPresenter.tsx
+++ src/features/AccountSettings/AccountSettingsPresenter.tsx
@@ -6,147 +6,179 @@ import IconButton from '@mui/material/IconButton';
 import { InfoOutlined } from '@mui/icons-material';
 import Tooltip from '@mui/material/Tooltip';
 import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
 
 export const AccountSettingsPresenter = () => {
   return (
     <>
       <Typography variant='h4'>アカウント情報</Typography>
 
-      {/** アカウント情報 */}
       <Box sx={{ display: 'flex', flexDirection: 'column', gap: '16px' }}>
-        {/** アバター */}
-        <Box sx={{ margin: '0 auto' }}>
-          <Box sx={{ position: 'relative' }}>
-            <Avatar sx={{ bgcolor: 'magenta' }}>HN</Avatar>
-            <IconButton
+        {/** アカウント情報 */}
+        <Box sx={{ display: 'flex', flexDirection: 'column', gap: '16px' }}>
+          {/** アバター */}
+          <Box sx={{ margin: '0 auto' }}>
+            <Box sx={{ position: 'relative' }}>
+              <Avatar sx={{ bgcolor: 'magenta' }}>HN</Avatar>
+              <IconButton
+                sx={{
+                  padding: 0,
+                  width: 'fit-content',
+                  position: 'absolute',
+                  bottom: -4,
+                  right: -8,
+                }}
+              >
+                <EditIcon sx={{ color: 'black' }} />
+              </IconButton>
+            </Box>
+          </Box>
+
+          {/** ペンネーム */}
+          <Box
+            sx={{
+              display: 'flex',
+              alignItems: 'baseline',
+              justifyContent: 'space-between',
+              gap: '16px',
+            }}
+          >
+            <Box
               sx={{
-                padding: 0,
-                width: 'fit-content',
-                position: 'absolute',
-                bottom: -4,
-                right: -8,
+                display: 'flex',
+                alignItems: 'center',
+                gap: '8px',
+                flexShrink: 0,
               }}
             >
-              <EditIcon sx={{ color: 'black' }} />
-            </IconButton>
-          </Box>
-        </Box>
+              <Typography>ペンネーム</Typography>
+              <Tooltip title='ペンネームはストーリー投稿時に表示します'>
+                <IconButton sx={{ padding: 0 }}>
+                  <InfoOutlined />
+                </IconButton>
+              </Tooltip>
+            </Box>
 
-        <Box
-          sx={{
-            display: 'flex',
-            alignItems: 'baseline',
-            justifyContent: 'space-between',
-            gap: '16px',
-          }}
-        >
+            <Box sx={{ display: 'flex', alignItems: 'flex-end', gap: '8px' }}>
+              <Typography sx={{ wordBreak: 'break-word' }}>
+                花子花花花花花花花花花花花花花花花花花花花花花花花花花花花花花花
+              </Typography>
+              <IconButton sx={{ padding: 0, width: 'fit-content' }}>
+                <EditIcon sx={{ color: 'black' }} />
+              </IconButton>
+            </Box>
+          </Box>
+
+          {/** ニックネーム */}
+          <Box
+            sx={{
+              display: 'flex',
+              alignItems: 'baseline',
+              justifyContent: 'space-between',
+              gap: '16px',
+            }}
+          >
+            <Box
+              sx={{
+                display: 'flex',
+                alignItems: 'center',
+                gap: '8px',
+                flexShrink: 0,
+              }}
+            >
+              <Typography>ニックネーム</Typography>
+              {/** // TODO：tooltipの内容は、コメント投稿機能実装時に修正 */}
+              <Tooltip title='ニックネーム'>
+                <IconButton sx={{ padding: 0 }}>
+                  <InfoOutlined />
+                </IconButton>
+              </Tooltip>
+            </Box>
+
+            <Box sx={{ display: 'flex', alignItems: 'flex-end', gap: '8px' }}>
+              <Typography sx={{ wordBreak: 'break-word' }}>
+                HANAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+              </Typography>
+              <IconButton sx={{ padding: 0, width: 'fit-content' }}>
+                <EditIcon sx={{ color: 'black' }} />
+              </IconButton>
+            </Box>
+          </Box>
+
+          {/** 生年月 */}
           <Box
             sx={{
               display: 'flex',
               alignItems: 'center',
-              gap: '8px',
-              flexShrink: 0,
+              justifyContent: 'space-between',
+              gap: '16px',
             }}
           >
-            <Typography>ペンネーム</Typography>
-            <Tooltip title='ペンネームはストーリー投稿時に表示します'>
-              <IconButton sx={{ padding: 0 }}>
-                <InfoOutlined />
-              </IconButton>
-            </Tooltip>
+            <Box sx={{ display: 'flex', alignItems: 'center', gap: '8px' }}>
+              <Typography>生年月</Typography>
+              <Tooltip title='生年月は一度登録したら変更できません'>
+                <IconButton sx={{ padding: 0 }}>
+                  <InfoOutlined />
+                </IconButton>
+              </Tooltip>
+            </Box>
+
+            <Typography>1998/2</Typography>
           </Box>
 
-          <Box sx={{ display: 'flex', alignItems: 'flex-end', gap: '8px' }}>
-            <Typography sx={{ wordBreak: 'break-word' }}>
-              花子花花花花花花花花花花花花花花花花花花花花花花花花花花花花花花
-            </Typography>
-            <IconButton sx={{ padding: 0, width: 'fit-content' }}>
-              <EditIcon sx={{ color: 'black' }} />
-            </IconButton>
-          </Box>
-        </Box>
-
-        <Box
-          sx={{
-            display: 'flex',
-            alignItems: 'baseline',
-            justifyContent: 'space-between',
-            gap: '16px',
-          }}
-        >
+          {/** 匿名設定 */}
           <Box
             sx={{
               display: 'flex',
               alignItems: 'center',
-              gap: '8px',
-              flexShrink: 0,
+              justifyContent: 'space-between',
+              gap: '16px',
             }}
           >
-            <Typography>ニックネーム</Typography>
-            {/** // TODO：tooltipの内容は、コメント投稿機能実装時に修正 */}
-            <Tooltip title='ニックネーム'>
-              <IconButton sx={{ padding: 0 }}>
-                <InfoOutlined />
-              </IconButton>
-            </Tooltip>
-          </Box>
+            <Box sx={{ display: 'flex', alignItems: 'center', gap: '8px' }}>
+              <Typography>匿名設定</Typography>
+              <Tooltip title='匿名設定をONにすると投稿は匿名で表示されます'>
+                <IconButton sx={{ padding: 0 }}>
+                  <InfoOutlined />
+                </IconButton>
+              </Tooltip>
+            </Box>
 
-          <Box sx={{ display: 'flex', alignItems: 'flex-end', gap: '8px' }}>
-            <Typography sx={{ wordBreak: 'break-word' }}>
-              HANAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
-            </Typography>
-            <IconButton sx={{ padding: 0, width: 'fit-content' }}>
-              <EditIcon sx={{ color: 'black' }} />
-            </IconButton>
+            <Switch />
           </Box>
         </Box>
 
-        <Box
-          sx={{
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'space-between',
-            gap: '16px',
-          }}
-        >
-          <Box sx={{ display: 'flex', alignItems: 'center', gap: '8px' }}>
-            <Typography>生年月</Typography>
-            <Tooltip title='生年月は一度登録したら変更できません'>
-              <IconButton sx={{ padding: 0 }}>
-                <InfoOutlined />
-              </IconButton>
-            </Tooltip>
-          </Box>
-
-          <Typography>1998/2</Typography>
+        {/** アカウント削除ボタン */}
+        <Box sx={{ textAlign: 'center' }}>
+          <Button variant='contained' sx={{ backgroundColor: '#F24726' }}>
+            アカウント削除
+          </Button>
         </Box>
 
-        <Box
-          sx={{
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'space-between',
-            gap: '16px',
-          }}
-        >
-          <Box sx={{ display: 'flex', alignItems: 'center', gap: '8px' }}>
-            <Typography>匿名設定</Typography>
-            <Tooltip title='匿名設定をONにすると投稿は匿名で表示されます'>
+        {/** 投稿小説 */}
+        <Box>
+          <Box sx={{ display: 'flex', gap: '8px', alignItems: 'center' }}>
+            <Typography>投稿小説</Typography>
+            <Tooltip title='自分が投稿した小説です'>
               <IconButton sx={{ padding: 0 }}>
                 <InfoOutlined />
               </IconButton>
             </Tooltip>
           </Box>
+        </Box>
 
-          <Switch />
+        {/** 読者登録小説（閲覧小説とかの表現の方がベターかも） */}
+        <Box>
+          <Box sx={{ display: 'flex', gap: '8px', alignItems: 'center' }}>
+            <Typography>閲覧小説</Typography>
+            <Tooltip title='自分が読んだ小説です'>
+              <IconButton sx={{ padding: 0 }}>
+                <InfoOutlined />
+              </IconButton>
+            </Tooltip>
+          </Box>
         </Box>
       </Box>
-
-      {/** アカウント削除ボタン */}
-
-      {/** 投稿小説 */}
-
-      {/** 読者登録小説（閲覧小説とかの表現の方がベターかも） */}
     </>
   );
 };
