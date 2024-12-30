cbbefd6c63624f0f3be3d1bb9715cc6e7294932e
azufb <curiousazunas@gmail.com>
2024-12-30 21:39:47 +0900

    スキーマ修正

--- src/features/AccountSettings/AccountSettings.schema.ts
+++ src/features/AccountSettings/AccountSettings.schema.ts
@@ -3,8 +3,9 @@ import { z } from 'zod';
 export const AccountSettingFormSchema = z.object({
   penName: z.string(),
   nickName: z.string(),
-  birth: z.date(),
+  birthYearAndMonth: z.date(),
   isAnonymous: z.boolean(),
+  profileIconImage: z.string(),
 });
 
 export type AccountSettingFormType = z.infer<typeof AccountSettingFormSchema>;
