44d8cf502151ca963609681211920d3a46d2bc24
Kanepion <pantera.yk37@gmail.com>
2025-02-13 17:57:34 +0900

    wip; ビルドした

--- src/components/novelcard/NovelCard.d.ts
+++ src/components/novelcard/NovelCard.d.ts
@@ -12,7 +12,7 @@ interface NovelCardProps {
   evaluation_stay_count: number;
   setEvaluation_stay_count: React.Dispatch<React.SetStateAction<number>>;
   novel: NovelProps;
-  onClick: () => void;
+  onClick: (sentence_id: number) => void;
 }
 declare const NovelCard: ({
   novel,
--- src/types/types.d.ts
+++ src/types/types.d.ts
@@ -1,7 +1,25 @@
 export interface Sentence {
   title: string;
+  main_copy: string;
+  overview: string;
+  popular: boolean;
+  newArrival: boolean;
+  author_user_name: string;
+  chips: JSX.Element[];
+  tags: JSX.Element[];
+  reader_count: number;
+  avatar: {
+    src: string;
+    alt: string;
+    color: string;
+    text: string;
+  };
   sentence_id: number;
+  sentence_user_count: number;
+  sentence_hierarchy_count: number;
   sentence: string;
+  text: string;
+  textIndex: number;
   userId: number;
   userName: string;
   profile_icon_image: string;
@@ -98,9 +116,9 @@ export interface NovelViewPresentationProps {
   textCount: number;
 }
 export interface NovelProps {
+  title: string;
   main_copy: string;
   overview: string;
-  title: string;
   popular: boolean;
   newArrival: boolean;
   avatar: {
@@ -111,10 +129,22 @@ export interface NovelProps {
   };
   author_user_name: string;
   chips: JSX.Element[];
+  created_at: string;
   tags: JSX.Element[];
   reader_count: number;
   updated_at: string;
+  sentence_id: number;
+  sentence: string;
+  userId: number;
+  userName: string;
+  profile_icon_image: string;
+  evaluation_good_count: number;
+  evaluation_stay_count: number;
   sentence_user_count: number;
   sentence_hierarchy_count: number;
   text: string;
+  textIndex: number;
+  children: Sentence[];
+  parent: Sentence[];
+  main: Sentence[];
 }
