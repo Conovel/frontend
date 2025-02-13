c191116876031edb730ccb4b66fe2d70d455e16f
Kanepion <pantera.yk37@gmail.com>
2025-02-13 18:38:19 +0900

    アカウント画面に枠追加など

--- src/features/AccountSettings/AccountSettings.d.ts
+++ src/features/AccountSettings/AccountSettings.d.ts
@@ -1,1 +1,1 @@
-export declare const AccountSettings: () => import('react/jsx-runtime').JSX.Element;
+export declare const AccountSettings: React.FC;
--- src/features/AccountSettings/AccountSettings.tsx
+++ src/features/AccountSettings/AccountSettings.tsx
@@ -5,8 +5,9 @@ import {
 } from './AccountSettingsPresenter';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { AccountSettingFormSchema } from './AccountSettings.schema';
+import { Box, Paper } from '@mui/material';
 
-export const AccountSettings = () => {
+export const AccountSettings: React.FC = () => {
   const methods = useForm({
     mode: 'onBlur', // TODO：アカウント情報の更新処理次第
     resolver: zodResolver(AccountSettingFormSchema),
@@ -24,30 +25,49 @@ export const AccountSettings = () => {
   };
 
   return (
-    <FormProvider {...methods}>
-      <AccountSettingsPresenter
-        accountInfo={accountInfo}
-        isEdit={false}
-        onChangeEditMode={() => {
-          // TODO:あとで実装
+    <Box
+      sx={{
+        maxWidth: '1200px',
+        margin: '0 auto',
+        padding: {
+          xs: '16px', // 450px未満での余白
+          sm: '16px calc((100% - 450px) / 2)', // 450px以上での余白
+        },
+      }}
+    >
+      <Paper
+        elevation={2}
+        sx={{
+          padding: '24px',
+          borderRadius: '8px',
         }}
-        onClickUpdateAccountInfo={() => {
-          // TODO：あとで実装
-        }}
-        onClickGoToMyPostedNovels={() => {
-          // TODO：あとで実装
-        }}
-        onClickGoToMyReadingNovels={() => {
-          // TODO：あとで実装
-        }}
-        onClickOpenDeleteAccountModal={() => {
-          // TODO：あとで実装
-        }}
-        isOpenDeleteAccountModal={false}
-        onCloseDeleteAccountModal={() => {
-          // TODO：あとで実装
-        }}
-      />
-    </FormProvider>
+      >
+        <FormProvider {...methods}>
+          <AccountSettingsPresenter
+            accountInfo={accountInfo}
+            isEdit={false}
+            onChangeEditMode={() => {
+              // TODO:あとで実装
+            }}
+            onClickUpdateAccountInfo={() => {
+              // TODO：あとで実装
+            }}
+            onClickGoToMyPostedNovels={() => {
+              // TODO：あとで実装
+            }}
+            onClickGoToMyReadingNovels={() => {
+              // TODO：あとで実装
+            }}
+            onClickOpenDeleteAccountModal={() => {
+              // TODO：あとで実装
+            }}
+            isOpenDeleteAccountModal={false}
+            onCloseDeleteAccountModal={() => {
+              // TODO：あとで実装
+            }}
+          />
+        </FormProvider>
+      </Paper>
+    </Box>
   );
 };
