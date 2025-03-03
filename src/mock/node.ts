3384d72b77a9e970680760ec1acacb4ce6e2c2b5
azufb <curiousazunas@gmail.com>
2025-03-03 23:20:08 +0900

    mock処理を上位に持ってくる

--- src/mock/node.ts
+++ src/mock/node.ts
@@ -1,4 +1,4 @@
 import { setupServer } from 'msw/node';
-import { handlers } from './handlers';
+import { handlers } from '../features/NovelList/mocks/handlers';
 
 export const server = setupServer(...handlers);
