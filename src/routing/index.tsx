899561254afcce461f2d7dd2d4ceccead579cb16
Kanepion <pantera.yk37@gmail.com>
2025-03-13 08:33:03 +0900

    pageディレクトリdeleteAccounts単数形対応

--- src/routing/index.tsx
+++ src/routing/index.tsx
@@ -6,7 +6,7 @@ import { Footer } from '../components/footer';
 import { TermsPage } from '../pages/terms';
 import { Box } from '@mui/material';
 import { AccountSettings } from '../features/AccountSettings/AccountSettings';
-import { DeleteAccountPage } from '../pages/deleteAccounts';
+import { DeleteAccountPage } from '../pages/deleteAccount';
 import { AccountDeleted } from '../features/DeleteAccount/AccountDeleted';
 import Login from '../pages/login';
 import { NovelViewContainer } from '../features/NovelView/NovelViewContainer';
