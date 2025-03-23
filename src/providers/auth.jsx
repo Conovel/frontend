448f346af49795bf157ae13e1dd0fc515f60345a
ryo-i <iidazz0109@gmail.com>
2025-03-24 07:36:57 +0900

    auth.jsxをフォーマット

--- src/providers/auth.jsx
+++ src/providers/auth.jsx
@@ -1,23 +1,23 @@
-import React, { createContext, useContext, useEffect, useState } from "react";
-import { API_URL } from "../config/settings";
+import React, { createContext, useContext, useEffect, useState } from 'react';
+import { API_URL } from '../config/settings';
 
 const AuthContext = createContext();
 export const useAuth = () => useContext(AuthContext);
 
 export const AuthProvider = ({ children }) => {
-  const [token, setToken] = useState("");
+  const [token, setToken] = useState('');
   const [currentUser, setCurrentUser] = useState(null);
 
   useEffect(() => {
     // URLからクエリパラメータを解析してトークンを取得
     const query = new URLSearchParams(window.location.search);
-    const tokenFromUrl = query.get("token");
+    const tokenFromUrl = query.get('token');
 
     if (tokenFromUrl) {
       setToken(tokenFromUrl);
-      localStorage.setItem("authToken", tokenFromUrl); // トークンをlocalStorageに保存
+      localStorage.setItem('authToken', tokenFromUrl); // トークンをlocalStorageに保存
     } else {
-      const storedToken = localStorage.getItem("authToken");
+      const storedToken = localStorage.getItem('authToken');
       if (storedToken) {
         setToken(storedToken);
       }
@@ -33,24 +33,22 @@ export const AuthProvider = ({ children }) => {
       })
         .then((response) => {
           if (!response.ok) {
-            throw new Error("Failed to fetch user");
+            throw new Error('Failed to fetch user');
           }
           return response.json();
         })
         .then((data) => setCurrentUser(data.user))
         .catch((error) => {
-          console.error("Error fetching user:", error);
+          console.error('Error fetching user:', error);
           logout(); // エラー時にログアウト
         });
     }
   }, [token]);
 
-  
-
   const logout = () => {
     setCurrentUser(null); // ユーザー情報をクリア
-    setToken(""); // トークンをクリア
-    localStorage.removeItem("authToken"); // localStorageからトークンを削除
+    setToken(''); // トークンをクリア
+    localStorage.removeItem('authToken'); // localStorageからトークンを削除
   };
 
   return (
