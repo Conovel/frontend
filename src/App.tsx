c96ad989cdc69c3664f97134da13dc34ef80db47
ryo-i <iidazz0109@gmail.com>
2025-04-02 15:05:04 +0900

    clientIdで環境変数変数を読み込む

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,13 @@ import { Routing } from './routing';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 // import { AuthProvider } from "./providers/auth"; // Google認証機能に使用予定
 
+const clientId = import.meta.env.VITE_GOOGLE_CLIENT_ID;
+console.log('clientId:', clientId);
+
 export default function App() {
   return (
     // デモ用のダミーIDを使用（実際の認証は機能しません）
-    <GoogleOAuthProvider clientId='dummy-client-id-for-demo'>
+    <GoogleOAuthProvider clientId={clientId}>
       {/* <AuthProvider> */}
         <Routing />
       {/* </AuthProvider> */}
