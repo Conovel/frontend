0bba9fb0d120fb2172687f519b4f9c3b05412277
ryo-i <iidazz0109@gmail.com>
2025-05-30 08:15:32 +0900

    Login画面で使わなくなった処理を削除

--- src/features/Login/LoginPresenter.tsx
+++ src/features/Login/LoginPresenter.tsx
@@ -3,11 +3,6 @@ import {
   Typography,
   Container,
   Button,
-  // Dialog,
-  // DialogTitle,
-  // DialogContent,
-  // DialogActions,
-  // TextField,
   Divider,
 } from '@mui/material';
 import { useAuth } from '../../providers/auth';
@@ -15,10 +10,8 @@ import { useState, useEffect } from 'react';
 import { useNavigate, Link } from 'react-router';
 
 export const LoginPresenter = () => {
-  const [loginError, setLoginError] = useState<string | null>(null);
-  // const [isFirstTimeLoginModalOpen, setIsFirstTimeLoginModalOpen] =
+  // const [loginError, setLoginError] = useState<string | null>(null);
   useState(false);
-  // const [username, setUsername] = useState('');
   const navigate = useNavigate();
   const authBaseUrl = import.meta.env.VITE_AUTH_BASE_URL;
   const { setToken, currentUserId, logout } = useAuth();
@@ -37,24 +30,6 @@ export const LoginPresenter = () => {
     window.location.href = `${authBaseUrl}/auth/google_oauth2`;
   };
 
-  // const handleCompleteOnboarding = () => {
-  //   // ユーザー名など初回設定情報を保存する処理をここに追加
-  //   console.log('初回設定が完了しました。ユーザー名:', username);
-  //   setIsFirstTimeLoginModalOpen(false);
-
-  //   // ログイン完了後にホームページなどに遷移
-  //   navigate('/');
-  // };
-
-  // const handleSkipOnboarding = () => {
-  //   setIsFirstTimeLoginModalOpen(false);
-  //   navigate('/');
-  // };
-
-  // const openFirstTimeLoginModal = () => {
-  //   setIsFirstTimeLoginModalOpen(true);
-  // };
-
   return (
     <Container maxWidth='sm'>
       <Box
@@ -71,10 +46,6 @@ export const LoginPresenter = () => {
         </Typography>
 
         <>
-          {/* 
-          　ログイン済みの場合はログアウトボタンとマイページへのリンクを表示
-          　そうでない場合はログインボタンを表示
-          */}
           {console.log('currentUserId:', currentUserId)}
           {currentUserId ? (
             <>
@@ -94,8 +65,6 @@ export const LoginPresenter = () => {
               </Link>
             </>
           ) : (
-            /* MEMO：ここに新規登録とログインを兼ねたボタンとわかるテキストを追加する？ */
-            /* MEMO：ボタン押下→Googleアカウント選択→既存ユーザーならログイン、なければユーザー追加 */
             <Button
               variant='outlined'
               color='primary'
@@ -107,56 +76,8 @@ export const LoginPresenter = () => {
           )}
         </>
 
-        {/* MEMO：新規登録とログインボタンを統合する方針の場合、下記のボタンは削除する */}
-        {/* <Button
-          variant='outlined'
-          color='primary'
-          onClick={openFirstTimeLoginModal}
-          sx={{ width: '240px' }}
-        >
-          初回ログイン
-        </Button> */}
-
-        {loginError && <Typography color='error'>{loginError}</Typography>}
+        {/* {loginError && <Typography color='error'>{loginError}</Typography>} */}
       </Box>
-
-      {/* 初回ログイン用モーダル */}
-      {/* <Dialog
-        open={isFirstTimeLoginModalOpen}
-        onClose={() => setIsFirstTimeLoginModalOpen(false)}
-      >
-        <DialogTitle>ようこそ！初回設定</DialogTitle>
-        <DialogContent>
-          <Box sx={{ mt: 2, display: 'flex', flexDirection: 'column', gap: 3 }}>
-            <Typography>
-              Conovelへようこそ！より良い体験のために、いくつかの設定をお願いします。
-            </Typography>
-
-            <TextField
-              label='ユーザー名'
-              variant='outlined'
-              fullWidth
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-              helperText='あなたの活動に表示される名前です'
-            />
-
-            <Typography variant='body2' color='text.secondary'>
-              この設定はあとからいつでも変更できます。
-            </Typography>
-          </Box>
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleSkipOnboarding}>スキップ</Button>
-          <Button
-            onClick={handleCompleteOnboarding}
-            variant='contained'
-            disabled={!username.trim()}
-          >
-            設定を完了
-          </Button>
-        </DialogActions>
-      </Dialog> */}
     </Container>
   );
 };
