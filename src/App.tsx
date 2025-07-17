0f58c3c30177efc584f1c5e90f27b0481b14e53e
Kanepion <pantera.yk37@gmail.com>
2025-07-17 18:45:47 +0900

    routerの問題を解決

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import './App.css';
 import { Routing } from './routing';
 import { GoogleOAuthProvider } from '@react-oauth/google';
+import { BrowserRouter } from 'react-router-dom';
 
 export default function App() {
   return (
     <GoogleOAuthProvider clientId='dummy-client-id-for-demo'>
-      <Routing />
+      <BrowserRouter>
+        <Routing />
+      </BrowserRouter>
     </GoogleOAuthProvider>
   );
 }
