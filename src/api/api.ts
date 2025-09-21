f6987389400a756c140e48b069c880b6b7dc6052
ryo-i <iidazz0109@gmail.com>
2025-09-22 08:42:38 +0900

    ログアウト状態が保持されるようにクッキーを送る

--- src/api/api.ts
+++ src/api/api.ts
@@ -753,7 +753,10 @@ export const AuthApiFp = function (configuration?: Configuration) {
     ): Promise<
       (axios?: AxiosInstance, basePath?: string) => AxiosPromise<void>
     > {
-      const localVarAxiosArgs = await localVarAxiosParamCreator.logOut(options);
+      const localVarAxiosArgs = await localVarAxiosParamCreator.logOut({
+        ...options,
+        withCredentials: true,
+      }); // 認証時にクッキーを送る（手動で修正）
       const localVarOperationServerIndex = configuration?.serverIndex ?? 0;
       const localVarOperationServerBasePath =
         operationServerMap['AuthApi.logOut']?.[localVarOperationServerIndex]
