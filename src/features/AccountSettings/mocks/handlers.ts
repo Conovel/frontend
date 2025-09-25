32b603d59d188812dc82c4c898721733a965bd98
Kanepion <pantera.yk37@gmail.com>
2025-09-25 19:53:09 +0900

    v1つけた

--- src/features/AccountSettings/mocks/handlers.ts
+++ src/features/AccountSettings/mocks/handlers.ts
@@ -1,7 +1,8 @@
 import { http, HttpResponse } from 'msw';
 import { mockUserData } from './data';
 
-const apiBaseUrl = import.meta.env.VITE_API_BASE_URL || 'http://localhost:3001';
+const apiBaseUrl =
+  import.meta.env.VITE_API_BASE_URL || 'http://localhost:3001/v1';
 console.log('MSW API Base URL:', apiBaseUrl);
 
 export const accountSettingsHandlers = [
