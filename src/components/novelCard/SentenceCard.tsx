526288d30cced5da4a9d304de05d3a4746adfe8c
Kanepion <yuya_kaneko@kanepion.com>
2025-11-17 00:00:43 +0900

    Children投稿クリックで遷移できるよう修正

--- src/components/novelCard/SentenceCard.tsx
+++ src/components/novelCard/SentenceCard.tsx
@@ -118,7 +118,7 @@ const SentenceCard = ({
         cursor: !canEvaluate ? 'not-allowed' : 'pointer',
       }}
       onClick={() => {
-        if (canEvaluate && onClick) {
+        if (onClick) {
           onClick();
         }
       }}
